09ed3ab11700d46db4634c2d82df70ad4e65cbeb
Thayane Lima <130837321+Thayane25@users.noreply.github.com>
2024-11-20 22:51:29 +0000

    Reset do projeto

--- gmt/components/MeuBotao.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { Button } from 'react-native';
-
-export default function MeuBotao({ titulo, onPress }) {
-  return <Button title={titulo} onPress={onPress} />;
-}
